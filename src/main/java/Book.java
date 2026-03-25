2778467ba2d7e0067ccf86675484ea1378dbad33
acamberos1 <camberos.athian1@gmail.com>
2026-03-24 19:14:22 -0700

    no methods yet

---
+++ src/main/java/Book.java
@@ -0,0 +1,28 @@
+import java.time.LocalDate;
+import java.util.*;
+
+
+public class Book {
+    // fields and methods go here
+public static final int ISBN_ =0;
+
+    public static final int TITLE_ =1;
+    public static final int SUBJECT_ =2;
+    public static final int PAGE_COUNT_ =3;
+    public static final int AUTHOR_ =4;
+    public static final int DUE_DATE_ =5;
+
+
+    private String isbn;
+    private String title;
+    private String subject;
+    private int pageCount;
+    private String author;
+    private LocalDate dueDate;
+
+
+
+
+
+}
+// add fields and methods and the problem should be fixed
